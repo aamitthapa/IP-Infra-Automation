304649454e65f61d9fa2019c874ddb8d63bf3490
aamitthapa <062amit@gmail.com>
2020-04-06 20:53:01 -0700

    Update load_test_set_php.py

--- load_test_set_php.py
+++ load_test_set_php.py
@@ -183,11 +183,12 @@ exec('python /var/www/html/test.py');
 '''
 test_set_php.write(test_set_php_base)
 test_set_ls=os.system("ls -l Test_Set/ | grep FULL")
-print test_set_ls
-test_set_no=test_set_ls.split("\n")
-#for test_set in test_set_no:
-#        print test_set.split(" ")[8].split("FULL")[0]
-#        test_set_php.write()
+#print test_set_ls
+test_set_no=str(test_set_ls).split("\n")
+for test_set in test_set_no:
+        if "FULL" in test_set:
+            print test_set.split(" ")[8].split("FULL")[0]
+#            test_set_php.write()
 
 #         <button><h4>CGNAT_CONSUMER</h4></button>
 #         <button><h4>CGNAT_RESELLER</h4></button>
