b4a4d145ea705c3fa91c2572652477fa2734b13a
aamitthapa <062amit@gmail.com>
2020-04-06 21:55:33 -0700

    Update load_test_set_php.py

--- load_test_set_php.py
+++ load_test_set_php.py
@@ -201,9 +201,9 @@ for test_set in test_set_no:
         if "FULL" in test_set:
             test_set_1=test_set.split(" ")
             test_set_2=test_set_1[9].split("_FULL")
-            test_set_php.write("<button<h4>"+test_set_2[0]+"</h4></button>\n")
-#            if i%3==0:
-#                test_set_php.write("<br>")
+            test_set_php.write("<button><h4>"+test_set_2[0]+"</h4></button>\n")
+            if i%3==0:
+                test_set_php.write("<br>")
             i=i+1
 test_set_php.write(test_set_php_end)
 
