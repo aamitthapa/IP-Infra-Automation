73a6a38e8b5ee0431da86cb892484740d7ad9c0f
aamitthapa <062amit@gmail.com>
2020-04-07 11:39:58 -0700

    Update load_test_set_php.py

--- load_test_set_php.py
+++ load_test_set_php.py
@@ -211,7 +211,8 @@ for test_set in test_set_no:
         if "FULL" in test_set:
             test_set_1=test_set.split(" ")
             test_set_2=test_set_1[9].split("_TEST_SET_FULL")
-            test_set_php.write("<button><h4>"+test_set_2[0]+"</h4></button>\n")
+            <button href='<?php CGNAT_CONSUMER_TEST_SET_FULL(); ?>'><h4>
+            test_set_php.write("<button href='<?php"+test_set_2+"_TEST_SET_FULL.py"+"'><h4>"+test_set_2[0]+"</h4></button>\n")
 #            if i%3==0:
 #                test_set_php.write("<br><br>")
 test_set_php.write(test_set_php_end)
