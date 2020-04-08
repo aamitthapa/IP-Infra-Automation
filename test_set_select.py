8eeba9672cf6129013512fd3ff50cbd117736383
aamitthapa <062amit@gmail.com>
2020-04-08 15:09:43 -0700

    Update test_set_select.py

--- test_set_select.py
+++ test_set_select.py
@@ -7,16 +7,16 @@ import getpass
 import xml.etree.ElementTree as ET
 import socket
 test_set_full=open('Test_Set/CGNAT_CONSUMER_TEST_SET_FULL.xml', "r")
-test_set_select=open('test_set_select')
-f=open("test_set.php", "w")
-f1=open("index_base.php", "r")
-f2=open("index_mid.php", "r")
-f3=open("index_end.php", "r")
+test_set_select=open('test_set_select', "r")
+#f=open("test_set.php", "w")
+#f1=open("index_base.php", "r")
+#f2=open("index_mid.php", "r")
+#f3=open("index_end.php", "r")
 for lines1 in test_set_full:
     for lines2 in test_set_select:
         if lines2 in lines1:
             print lines
-f.close()
-f1.close()
-f2.close()
-f3.close()
+#f.close()
+#f1.close()
+#f2.close()
+#f3.close()
