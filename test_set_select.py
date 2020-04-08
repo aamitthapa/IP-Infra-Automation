dd50172186905416792dcec6b491db19133d9dd0
aamitthapa <062amit@gmail.com>
2020-04-08 16:55:19 -0700

    Update test_set_select.py

--- test_set_select.py
+++ test_set_select.py
@@ -9,11 +9,10 @@ import socket
 test_set_full=open('Test_Set/CGNAT_CONSUMER_TEST_SET_FULL.xml', "r")
 test_set_select=open('test_set_select', "r")
 test_set_xml=open('Test_Set/test_set.xml', "w")
-#f=open("test_set.php", "w")
-#f1=open("index_base.php", "r")
-#f2=open("index_mid.php", "r")
-#f3=open("index_end.php", "r")
-#items=test_set_full.split("\n")
+f=open('test_set_select.php', "w")
+f1=open("index_base.php", "r")
+f2=open("index_mid.php", "r")
+f3=open("index_end.php", "r")
 test_set_xml.write('''<?xml version="1.0"?>
 <test_set> \n''')
 
@@ -25,7 +24,48 @@ for lines1 in test_set_full:
 
     test_set_select.close()
 test_set_xml.write('''</test_set>''')
-#f.close()
-#f1.close()
-#f2.close()
-#f3.close()
+
+test_set_xml.close()
+tree = ET.parse('Test_Set/test_set.xml')
+root = tree.getroot()
+
+for lines in f1:
+	f.write(lines)
+
+
+f.write('''
+if(isset($_POST['run']))//to run PHP script on run
+{
+    $fp=fopen('test_set_select','w');
+	if(!empty($_POST['test_cases'])) // Loop to store and display values of individual checked checkbox.
+      {
+        foreach($_POST['test_cases'] as $selected)
+        {
+		fwrite($fp, $selected);
+        fwrite($fp, "\n");
+        }
+	fclose($fp);
+      }
+}
+''')
+
+for lines in f2:
+	f.write(lines)
+f.write("<center><h3><label>Select Test Cases to Run</label></h3></center>")
+f.write('<form action="#" method="post" class="container">\n')
+f.write('''<input type="checkbox" name= "Select All" value="Select All"><label>Select All</label><br>\n''')
+
+
+for test_case in root.findall('test_case'):
+        test_case=test_case.attrib.get('name')
+        f.write('''<input type="checkbox" name= "test_cases[]" value="'''+test_case+'"'+'''><label>'''+ test_case+'''</label><br>\n''')
+f.write('''
+<input type="submit" name="run" value="Run"/>
+</form>''')
+
+for lines in f3:
+	f.write(lines)
+f.close()
+f1.close()
+f2.close()
+f3.close()
