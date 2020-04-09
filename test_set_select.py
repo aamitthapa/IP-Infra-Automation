18d30c890e5ee72479eef03e0cc8deaf77447c26
aamitthapa <062amit@gmail.com>
2020-04-09 13:05:30 -0700

    Update test_set_select.py

--- test_set_select.py
+++ test_set_select.py
@@ -8,7 +8,7 @@ import xml.etree.ElementTree as ET
 import socket
 test_set_full=open('Test_Set/CGNAT_CONSUMER_TEST_SET_FULL.xml', "r")
 test_set_select=open('test_set_select', "r")
-test_set_xml=open('Test_Set/test_set.xml', "w")
+test_set_xml=open('test_set.xml', "w")
 f=open('test_set_select.php', "w")
 f1=open("index_base.php", "r")
 f2=open("index_mid.php", "r")
