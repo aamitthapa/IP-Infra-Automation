52cece8fc7b7fdf8a7382a9559321b4ba394c805
aamitthapa <062amit@gmail.com>
2020-04-08 17:05:31 -0700

    Update CGNAT_CONSUMER_TEST_SET_FULL.py

--- CGNAT_CONSUMER_TEST_SET_FULL.py
+++ CGNAT_CONSUMER_TEST_SET_FULL.py
@@ -34,7 +34,7 @@ echo "test_set_select.php";
 for lines in f2:
 	f.write(lines)
 f.write("<center><h3><label>Select Test Cases to Run</label></h3></center>")
-f.write('<form action="#" method="post" class="container">\n')
+f.write('<form method="post" class="container">\n')
 f.write('''<input type="checkbox" name= "Select All" value="Select All"><label>Select All</label><br>\n''')
 for test_case in root.findall('test_case'):
         test_case=test_case.attrib.get('name')
