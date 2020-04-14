d5ed48d81ab38f510f7801be065c6e2462e7cd73
aamitthapa <062amit@gmail.com>
2020-04-14 14:49:04 -0700

    Update test_set_select.py

--- test_set_select.py
+++ test_set_select.py
@@ -47,7 +47,7 @@ f.write('''
 <div class="container">
 <div class="jumbotron">
 <center><h3><label>Test Logs</label></h3></center>
-<div class="scroll" id="response"></div>''')
+<div style="overflow: scroll; height:400px;" id="response"></div>''')
 for lines in f3:
 	f.write(lines)
 f.close()
