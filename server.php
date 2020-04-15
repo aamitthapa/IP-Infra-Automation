1fdc1d14b4e66dd48c007fdbebd0dc7f726bc0ad
aamitthapa <062amit@gmail.com>
2020-04-15 14:37:59 -0700

    Create server.php

---
+++ server.php
@@ -0,0 +1,42 @@
+<?php
+set_time_limit(0);
+
+// arquivo cujo conteúdo será enviado ao cliente
+$dataFileName = 'Test_Result/test_log';
+
+while ( true )
+{
+	$requestedTimestamp = isset ( $_GET [ 'timestamp' ] ) ? (int)$_GET [ 'timestamp' ] : null;
+
+	// o PHP faz cache de operações "stat" do filesystem. Por isso, devemos limpar esse cache
+	clearstatcache();
+	$modifiedAt = filemtime( $dataFileName );
+
+	if ( $requestedTimestamp == null || $modifiedAt > $requestedTimestamp )
+	{
+		$handle = fopen("Test_Result/test_log", "rb");
+		$data = stream_get_contents($handle);
+		fclose($handle);
+		$test= fopen("test_auto.txt", "w");
+		fwrite($test, $data);
+		fclose($test);
+	#	$data = file_get_contents( $dataFileName );
+
+		$arrData = array(
+			'content' => $data,
+			'timestamp' => $modifiedAt
+		);
+		$file = fopen("autoupdate.txt","w");
+		fwrite($file, $data);
+		$json = json_encode( $arrData );
+		fwrite($file, $json);
+		echo $json;
+
+		break;
+	}
+	else
+	{
+		sleep( 1 );
+		continue;
+	}
+}
