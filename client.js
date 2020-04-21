3096870a0aef7bac8c84e1c1aefa477d30db37a3
aamitthapa <062amit@gmail.com>
2020-04-21 14:53:20 -0700

    Update client.js

--- client.js
+++ client.js
@@ -1,21 +1,23 @@
 function getContent( timestamp )
 {
-        var queryString = { 'timestamp' : timestamp };
-        $.get ( 'server.php' , queryString , function ( data )
-        {
-                var obj = jQuery.parseJSON( data );
-                var myJSON = JSON.stringify(obj);
-                var check2 = myJSON.indexOf("Test Set Complete!!!!!!");
-//              window.alert(check2);
-                document.getElementById("response").innerHTML = obj.content.toString();
-                if (check2 !== -1) {
-                //      document.getElementById("complete").innerHTML = "Test Set Completed";
-                        window.location = "index.php"; }
-                getContent( obj.timestamp );
-        });
+	var queryString = { 'timestamp' : timestamp };
+	var check='Test Set Complete!!!!!!';
+	var check1='utsav';
+	$.get ( 'server.php' , queryString , function ( data )
+	{
+		var obj = jQuery.parseJSON( data );
+		var myJSON = JSON.stringify(obj);
+		var check2 = myJSON.indexOf("Test Set Complete!!!!!!");
+//		window.alert(check2);
+		document.getElementById("response").innerHTML = obj.content.toString();
+		if (check2 !== -1) {
+		//	document.getElementById("complete").innerHTML = "Test Set Completed";
+			window.location = "test_set_pre_complete.php"; }
+		getContent( obj.timestamp );
+	});
 }
 
 $( document ).ready ( function ()
 {
-        getContent();
+	getContent();
 });
