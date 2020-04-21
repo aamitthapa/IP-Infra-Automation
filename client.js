64f15f538287163084523a748efb748b6e958963
aamitthapa <062amit@gmail.com>
2020-04-20 22:20:02 -0700

    Update client.js

--- client.js
+++ client.js
@@ -1,16 +1,21 @@
 function getContent( timestamp )
 {
-	var queryString = { 'timestamp' : timestamp };
-	$.get ( 'server.php' , queryString , function ( data )
-	{
-		var obj = jQuery.parseJSON( data );
-		$( '#response' ).html( obj.content );
-		// reconecta ao receber uma resposta do servidor
-		getContent( obj.timestamp );
-	});
+        var queryString = { 'timestamp' : timestamp };
+        $.get ( 'server.php' , queryString , function ( data )
+        {
+                var obj = jQuery.parseJSON( data );
+                var myJSON = JSON.stringify(obj);
+                var check2 = myJSON.indexOf("Test Set Complete!!!!!!");
+//              window.alert(check2);
+                document.getElementById("response").innerHTML = obj.content.toString();
+                if (check2 !== -1) {
+                //      document.getElementById("complete").innerHTML = "Test Set Completed";
+                        window.location = "test_set_complete.php"; }
+                getContent( obj.timestamp );
+        });
 }
 
 $( document ).ready ( function ()
 {
-	getContent();
+        getContent();
 });
